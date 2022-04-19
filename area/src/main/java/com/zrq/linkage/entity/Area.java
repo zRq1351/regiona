180006b340ceef28a6ba5ee81e8fc9ad736c349d
zrq1351 <320401891@qq.com>
2022-04-19 22:27:37 +0800

    1

--- area/src/main/java/com/zrq/linkage/entity/Area.java
+++ area/src/main/java/com/zrq/linkage/entity/Area.java
@@ -27,99 +27,100 @@ public class Area implements Serializable {
 	@Transient
 	private Set<Area> areas = new HashSet<Area>();
 
+	public String getId() {
+		return id;
+	}
+
 	public void setId(String id) {
 		this.id = id;
 	}
 
-	public String getId() {
-		return this.id;
+	public String getName() {
+		return name;
 	}
 
 	public void setName(String name) {
 		this.name = name;
 	}
 
-	public String getName() {
-		return this.name;
+	public String getPid() {
+		return pid;
 	}
 
 	public void setPid(String pid) {
 		this.pid = pid;
 	}
 
-	public String getPid() {
-		return this.pid;
+	public String getProvinceCode() {
+		return provinceCode;
 	}
 
 	public void setProvinceCode(String provinceCode) {
 		this.provinceCode = provinceCode;
 	}
 
-	public String getProvinceCode() {
-		return this.provinceCode;
+	public String getCityCode() {
+		return cityCode;
 	}
 
 	public void setCityCode(String cityCode) {
 		this.cityCode = cityCode;
 	}
 
-	public String getCityCode() {
-		return this.cityCode;
+	public String getAreaCode() {
+		return areaCode;
 	}
 
 	public void setAreaCode(String areaCode) {
 		this.areaCode = areaCode;
 	}
 
-	public String getAreaCode() {
-		return this.areaCode;
+	public String getStreetCode() {
+		return streetCode;
 	}
 
 	public void setStreetCode(String streetCode) {
 		this.streetCode = streetCode;
 	}
 
-	public String getStreetCode() {
-		return this.streetCode;
+	public String getCommitteeCode() {
+		return committeeCode;
 	}
 
 	public void setCommitteeCode(String committeeCode) {
 		this.committeeCode = committeeCode;
 	}
 
-	public String getCommitteeCode() {
-		return this.committeeCode;
+	public String getCommitteeType() {
+		return committeeType;
 	}
 
 	public void setCommitteeType(String committeeType) {
 		this.committeeType = committeeType;
 	}
 
-	public String getCommitteeType() {
-		return this.committeeType;
+	public String getSort() {
+		return sort;
 	}
 
 	public void setSort(String sort) {
 		this.sort = sort;
 	}
 
-	public String getSort() {
-		return this.sort;
+	public String getLevel() {
+		return level;
 	}
 
 	public void setLevel(String level) {
 		this.level = level;
 	}
 
-	public String getLevel() {
-		return this.level;
-	}
-
 	public Set<Area> getAreas() {
-		return this.areas;
+		return areas;
 	}
 
 	public void setAreas(Set<Area> areas) {
 		this.areas = areas;
 	}
+
 }
